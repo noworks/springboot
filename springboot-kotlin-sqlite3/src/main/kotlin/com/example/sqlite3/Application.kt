dac3d6d0c79fbc91c792144293dba43f7f0adcca
izuno4t <tadashi.izuno@gmail.com>
2018-01-13 15:24:44 +0900

    add springboot-kotlin-sqlite3

---
+++ springboot-kotlin-sqlite3/src/main/kotlin/com/example/sqlite3/Application.kt
@@ -0,0 +1,11 @@
+package com.example.springbootkotlinsqlite3
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
